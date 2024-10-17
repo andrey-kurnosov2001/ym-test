0b8c02f056ff82e87e559df8ee1d28ef63fa4dab
Андрей Курносов <andrey.kurnosov@megafon.ru>
2024-10-17 19:11:23 +0300

    test5

--- src/pages/RedirectPage/RedirectPage.jsx
+++ src/pages/RedirectPage/RedirectPage.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useNavigate} from "react-router-dom";
 
 const RedirectPage = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate('/');
+    }, []);
+
     return (
         <div>
             <h2>Redirect Page</h2>
