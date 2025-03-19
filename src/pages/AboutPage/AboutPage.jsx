f39d452712b590e3b5efaab1117458d5f7190f45
Андрей Курносов <andrey.kurnosov@megafon.ru>
2025-03-19 16:33:30 +0300

    add Varioqub

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,18 +2,19 @@ import React, {useEffect, useState} from 'react';
 
 const AboutPage = () => {
     const [data, setData] = useState();
+    const dataFlags = sessionStorage.getItem('ymabFlags');
 
     useEffect(() => {
-        setData(window?.ymabFlags);
-        console.log('window?.ymabFlags', window?.ymabFlags);
-    }, [window?.ymabFlags]);
+        setData(dataFlags);
+        console.log('window?.ymabFlags', dataFlags);
+    }, [dataFlags]);
 
     console.log(data);
 
 
     return (
         <div>
-            <h2>About Page Varioqub 6</h2>
+            <h2>About Page Varioqub 7</h2>
         </div>
     );
 };
