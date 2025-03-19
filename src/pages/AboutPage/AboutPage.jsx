aa3a0502b7ec29d986d232b757514461abf6261e
Андрей Курносов <andrey.kurnosov@megafon.ru>
2025-03-19 16:27:19 +0300

    add Varioqub

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const AboutPage = () => {
 
+    console.log(window?.ymabFlags);
+
     return (
         <div>
-            <h2>About Page Varioqub 4</h2>
+            <h2>About Page Varioqub 5</h2>
         </div>
     );
 };
