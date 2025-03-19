b8038aab9f899aa227d901d377e8dd5b4f2cd1c9
Андрей Курносов <andrey.kurnosov@megafon.ru>
2025-03-19 16:48:10 +0300

    add Varioqub

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,21 +1,33 @@
 import React, {useEffect, useState} from 'react';
 
 const AboutPage = () => {
+    // const [data, setData] = useState();
+    //
+    // useEffect(() => {
+    //     window.ymab('metrika.98662215', 'getFlags', {}, (flags) => {
+    //         console.log('--------> Varioqub data', flags);
+    //         setData(flags);
+    //         console.log('--------> data', data);
+    //         sessionStorage.setItem('ymabFlags', JSON.stringify(flags));
+    //         console.log('--------> sessionStorage', sessionStorage.getItem('ymabFlags'));
+    //     });
+    // }, []);
+
     const [data, setData] = useState();
+    const flags = sessionStorage.getItem('ymabFlags');
 
     useEffect(() => {
-        window.ymab('metrika.98662215', 'getFlags', {}, (flags) => {
-            console.log('--------> Varioqub data', flags);
+        console.log('----> useEffect flags', flags);
+        if (flags) {
             setData(flags);
-            console.log('--------> data', data);
-            sessionStorage.setItem('ymabFlags', JSON.stringify(flags));
-            console.log('--------> sessionStorage', sessionStorage.getItem('ymabFlags'));
-        });
-    }, []);
+        }
+    }, [flags]);
+
+    console.log('----> AboutPage data', data);
 
     return (
         <div>
-            <h2>About Page Varioqub 9</h2>
+            <h2>About Page Varioqub 10</h2>
         </div>
     );
 };
