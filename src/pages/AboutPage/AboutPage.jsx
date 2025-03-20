9992a6e6cc59973e5fd18bc6d35f18c18dd44454
Андрей Курносов <andrey.kurnosov@megafon.ru>
2025-03-20 09:57:03 +0300

    variqub

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,34 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+import {MetricaExperimentsProvider, useExperiments} from "yandex-metrica-ab-react";
+
+const clientId = "metrika.98662215";
 
 const AboutPage = () => {
-    // const [data, setData] = useState();
-    //
-    // useEffect(() => {
-    //     window.ymab('metrika.98662215', 'getFlags', {}, (flags) => {
-    //         console.log('--------> Varioqub data', flags);
-    //         setData(flags);
-    //         console.log('--------> data', data);
-    //         sessionStorage.setItem('ymabFlags', JSON.stringify(flags));
-    //         console.log('--------> sessionStorage', sessionStorage.getItem('ymabFlags'));
-    //     });
-    // }, []);
-
-    const [data, setData] = useState();
-    const flags = sessionStorage.getItem('ymabFlags');
-
-    useEffect(() => {
-        console.log('----> useEffect flags', flags);
-        if (flags) {
-            setData(flags);
-        }
-    }, [flags]);
-
-    console.log('----> AboutPage data', data);
+    const { flags } = useExperiments({ clientId });
+    console.log(flags);
 
     return (
-        <div>
-            <h2>About Page Varioqub 10</h2>
-        </div>
+        <MetricaExperimentsProvider clientId={clientId}>
+            <div>
+                <h2>About Page Varioqub 11</h2>
+            </div>
+        </MetricaExperimentsProvider>
     );
 };
 
